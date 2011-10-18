3fc26b83b244caaf3ea7fa0443e5fff24a1e4c61
unknown <stevenrigney@.afni.net>
2011-10-17 21:58:36 -0500

    Setting up the first tests for the model.

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -5,9 +5,37 @@ import models.*;
 
 public class BasicTest extends UnitTest {
 
-    @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
-    }
+	@Before
+	public void setup() {
+		Fixtures.deleteAll();
+	}
+    
+   @Test
+   public void aVeryImportantThingToTest() {
+		assertEquals(2, 1 + 1);
+   }
+    
+	@Test
+	public void verifyEmptyDB() {
+		List<Topic> topics = Topic.findAll();
+		assertEquals(0, topics.size());
+   }
 
+	@Test
+	public void createAndRetrieveATopic() {
+		new Topic("this is awesome", new Date(), "Steve").save();
+		
+		Topic saved = Topic.find("byTopic", "this is awesome").first();
+		
+		assertNotNull(saved);
+		assertEquals("this is awesome", saved.topic);
+	}
+	
+	@Test
+	public void createAndRetrieveAPost() {
+		new Post(new Date(), "Steve", "I agree").save();
+		Post post = Post.find("byAuthor", "Steve").first();
+		assertNotNull(post);
+		assertEquals("I agree", post.comment);
+	}
 }
